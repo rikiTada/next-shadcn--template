d675b924948b701b50d0d2e03e83ab0a47d11352
rikiTada <riki.tada777@gmail.com>
2024-08-24 00:04:04 +0900

    [update] src dir

--- rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Enter the new name:"
-read new_name
-sed -i "s/\"name\":.*/\"name\": \"$new_name\",/" package.json
-sed -i "s/\"name\":.*/\"name\": \"$new_name\",/" package-lock.json
-
-echo "✨ Done."
